03241f3b0e31dea45e014fc0db9dbd77c38b12d5
Jichao Ouyang <oyanglulu@gmail.com>
2014-03-30 17:38:32 +0800

    remove header and clean style :lipstick:

--- src/blogist.js
+++ src/blogist.js
@@ -1,25 +1,11 @@
-function getToken(){
-	var token = localStorage.getItem("access_token");
-	if(token)
-		return "?access_token=" + token;
-	return "";
-}
 
 var username = $("meta[name=username]").attr("content");
-
-var userModel = new Model("user","get@https://api.github.com/user"+ getToken());
 
 var bloglistModel = new Model("bloglist","get@https://api.github.com/users/jcouyang/gists");
 
 var blogdetailModel = Model.extend({
 	dataOptions:{dataType:"jsonp"}
 });
-var HeaderView = View.extend({
-	model:userModel,
-	el: $(".navbar.navbar-default"),
-	template:"src/templates/header.html"
-});
-
 
 var BlogDetailView = View.extend({
 	el:$(".container .article"),
@@ -32,14 +18,11 @@ var BloglistView = View.extend({
 	template:"src/templates/gistlist.html"
 });
 
-var header = new HeaderView();
-header.render();
 var bloglist = new BloglistView();
 
 var router = new Router();
 router.get("/", function(){
 	console.log("homepage");
-	header.render();
 	bloglist.render();
 });
 
@@ -48,5 +31,5 @@ router.get("/gist/:gistid/?",function(params,data){
 });
 
 router.get("/gist/:gistid/.+",function(params,data){
-	new BlogDetailView({model:new blogdetailModel("blogdetail",'get@https://gist.github.com/'+username+'/'+ params.gistid +".json")}).render();
+	new BlogDetailView({model:new blogdetailModel(params.gistid,'get@https://gist.github.com/'+username+'/'+ params.gistid +".json")}).render();
 });
