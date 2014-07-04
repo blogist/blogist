30a865b4dcae08f6ab77953642143e360d28ae65
Jichao Ouyang <oyanglulu@gmail.com>
2014-07-04 22:43:06 +0800

    fix myblog build

--- configtravis.sh
+++ configtravis.sh
@@ -2,7 +2,7 @@ token=$(git config github.oauth-token)
 if [ -z $token ]
 then
 		echo "geting the token from github"
-		token=$(curl -u '' -d '{"scopes":["gist"], "note":"blogist"}' https://api.github.com/authorizations | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["token"]')
+		token=$(curl -u 'jcouyang' -d '{"scopes":["gist"], "note":"blogist"}' https://api.github.com/authorizations | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["token"]')
 fi
 gem install travis
 travis encrypt GH_TOKEN=$token --add
